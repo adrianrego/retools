e037561d6e4b5c8844bf23a3bbea26d1c6a4ec8b
Adrian Rego <arego320@gmail.com>
2013-06-30 15:47:28 -0500

    python 2.5 property hack

--- retools/__init__.py
+++ retools/__init__.py
@@ -16,6 +16,23 @@ from redis import Redis
 
 __all__ = ['Connection']
 
+# https://gist.github.com/romuald/1104222
+# Python 2.5 compatibility hack for property.setter, property.deleter
+if not hasattr(__builtins__.property, "setter"):
+    class property(__builtins__.property):
+        __metaclass__ = type
+
+        def setter(self, method):
+            return property(self.fget, method, self.fdel)
+
+        def deleter(self, method):
+            return property(self.fget, self.fset, method)
+
+        @__builtins__.property
+        def __doc__(self):
+            """Doc seems not to be set correctly when subclassing"""
+            return self.fget.__doc__
+
 
 class Connection(object):
     """The default Redis Connection
