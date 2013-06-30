5ed8b5d5102c587431d9290f4b8338818fba7003
Adrian Rego <arego320@gmail.com>
2013-06-30 16:01:46 -0500

    another py2.5 property attempt

--- retools/__init__.py
+++ retools/__init__.py
@@ -12,27 +12,9 @@ Alternatively, many parts of retools accept Redis instances that may be passed
 directly.
 
 """
-import __builtin__
 from redis import Redis
 
 __all__ = ['Connection']
-
-# https://gist.github.com/romuald/1104222
-# Python 2.5 compatibility hack for property.setter, property.deleter
-if not hasattr(__builtins__.property, "setter"):
-    class property(__builtins__.property):
-        __metaclass__ = type
-
-        def setter(self, method):
-            return property(self.fget, method, self.fdel)
-
-        def deleter(self, method):
-            return property(self.fget, self.fset, method)
-
-        @__builtins__.property
-        def __doc__(self):
-            """Doc seems not to be set correctly when subclassing"""
-            return self.fget.__doc__
 
 
 class Connection(object):
@@ -47,14 +29,14 @@ class Connection(object):
     def __init__(self):
         self._redis = None
 
-    @property
-    def redis(self):
+    def _get_redis(self):
         if not self._redis:
             self._redis = Redis()
         return self._redis
 
-    @redis.setter
-    def redis(self, conn):
+    def _set_redis(self, conn):
         self._redis = conn
 
+    redis = property(_get_redis, _set_redis)
+    
 global_connection = Connection()
